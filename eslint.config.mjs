14d79026e56072b3a1a5f791bfeb0fe5087edf9f
Justyna Gruba <justyna.gruba@deployed.pl>
2024-04-24 16:44:49 +0200

    Code review.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,31 @@
-import globals from 'globals'
-import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
+import globals from "globals";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
-import path from 'path'
-import { fileURLToPath } from 'url'
-import { FlatCompat } from '@eslint/eslintrc'
-import pluginJs from '@eslint/js'
-import prettier from 'eslint-plugin-prettier'
+import path from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+import pluginJs from "@eslint/js";
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
 // mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended })
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended,
+});
 
 export default [
   { languageOptions: { globals: globals.node } },
-  ...compat.extends('standard-with-typescript'),
+  ...compat.extends("standard-with-typescript"),
   pluginReactConfig,
-  { ignores: ['*.config.*', '.yarn', '.prettierrc.js'] },
+  { ignores: ["*.config.*", ".yarn", ".prettierrc.js"] },
+  eslintPluginPrettierRecommended,
+  ,
   {
-      plugins: {
-        prettier
-      },
-      rules: {
-        'semi': 'off',
-        '@typescript-eslint/semi': 'off',
-        'prettier/prettier': 'error',
-    } 
-  }
-] 
+    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
+    rules: {
+      semi: "off",
+      "@typescript-eslint/semi": "off",
+    },
+  },
+];
