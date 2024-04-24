a911dcc218f239cb20e77dd6143d990ecc66d7e1
Justyna Gruba <justyna.gruba@deployed.pl>
2024-04-24 10:26:59 +0200

    Husky added.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
 import pluginJs from '@eslint/js'
-
+import prettier from 'eslint-plugin-prettier'
 // mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -15,11 +15,15 @@ export default [
   { languageOptions: { globals: globals.node } },
   ...compat.extends('standard-with-typescript'),
   pluginReactConfig,
-  { ignores: ['*.config.*', '.yarn'] },
+  { ignores: ['*.config.*', '.yarn', '.prettierrc.js'] },
   {
+      plugins: {
+        prettier
+      },
       rules: {
         'semi': 'off',
-        "@typescript-eslint/semi": "off",
+        '@typescript-eslint/semi': 'off',
+        'prettier/prettier': 'error',
     } 
   }
 ] 
